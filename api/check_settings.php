a09800cb445b1d55cc502096e312d8f351e4e108
Swetha-papani2005 <papaniswetha@gmail.com>
2026-07-30 10:02:44 +0530

    cleanup: remove temporary debug files

--- api/check_settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'config.php';
-$conn = getConnection();
-$q = $conn->query("SELECT * FROM settings");
-while ($r = $q->fetch_assoc()) {
-    echo $r['key_name'] . ": " . $r['key_value'] . "\n";
-}
-?>
