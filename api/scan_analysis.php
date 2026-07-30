32b201022d28b14668637e8f30ce11b926b405c1
Swetha-papani2005 <papaniswetha@gmail.com>
2026-07-30 09:51:06 +0530

    debug: enable display_errors in scan_analysis.php

--- api/scan_analysis.php
+++ api/scan_analysis.php
@@ -1,5 +1,7 @@
-<?php
 require_once 'config.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 function detectCariesHeuristic($filepath, $type) {
     if (empty($filepath) || !file_exists($filepath)) {
